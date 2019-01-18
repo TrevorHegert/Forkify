8d1b7059038ebfa6932e4fa5515ffa425bcaef4b
Trevor Hegert <thegert138@gmail.com>
2019-01-18 16:27:37 -0500

    Babel Added according to change notes given

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import num from './test';
 
-console.log(`I imported ${num} from another module called test.js !`);
+console.log(`I imported some stuff from another module called test.js !`);
