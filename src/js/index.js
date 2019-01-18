646428294467db76ef9b9bfc15b4bc47aac003a8
Trevor Hegert <thegert138@gmail.com>
2019-01-18 16:36:21 -0500

    Babel successfully added to project

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import num from './test';
-
-console.log(`I imported some stuff from another module called test.js !`);
+const x = 23;
+console.log(`I imported ${num} from another module called test.js! Variable x is set to ${x}`);
