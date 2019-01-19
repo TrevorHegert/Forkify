d6298993d7a2a3a8a85b85c68249cb16520a6bfc
Trevor Hegert <thegert138@gmail.com>
2019-01-19 17:46:19 -0500

    Calling API to return recipes working

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,25 @@
-import str from './models/Search';
+//API key: 07dd95208f5ca6a6f32bc77083051683 
+//Search URL: https://www.food2fork.com/api/search
+//Get URL: https://www.food2fork.com/api/get
 
-// import {
-//     add as a,
-//     multiply as m,
-//     ID
-// } from './views/searchView';
+// key: 07dd95208f5ca6a6f32bc77083051683
+// q: (optional) Search Query (Ingredients should be separated by commas). If this is omitted top rated recipes will be returned.
+// sort: (optional) How the results should be sorted. See Below for details.
+// page: (optional) Used to get additional results
 
-import * as searchView from './views/searchView';
+import axios from 'axios';
 
-console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
+async function getResults(query) {
+    const proxy = 'https://cors.io/?';
+    const key = '07dd95208f5ca6a6f32bc77083051683';
+    try {
+        const result = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = result.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+
+}
+
+getResults('instant');
