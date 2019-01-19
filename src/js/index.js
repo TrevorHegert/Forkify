1ee384627ae318d914f1048e3b8887e4818b752e
Trevor Hegert <thegert138@gmail.com>
2019-01-19 18:00:51 -0500

    Put search results into a search object

--- src/js/index.js
+++ src/js/index.js
@@ -7,19 +7,8 @@
 // sort: (optional) How the results should be sorted. See Below for details.
 // page: (optional) Used to get additional results
 
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors.io/?';
-    const key = '07dd95208f5ca6a6f32bc77083051683';
-    try {
-        const result = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = result.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-
-}
-
-getResults('instant');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,20 @@
-export default 'I am an exported string.';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors.io/?';
+        const key = '07dd95208f5ca6a6f32bc77083051683';
+        try {
+            const result = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            console.log(result);
+            this.result = result.data.recipes;
+            console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
