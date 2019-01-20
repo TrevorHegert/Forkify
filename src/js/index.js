195ef02306130ef2aa5b6938b9e3a79ab91f2e31
Trevor Hegert <thegert138@gmail.com>
2019-01-20 14:27:49 -0500

    Empty State Object created, roadmap begun

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,14 @@
 
 import Search from './models/Search';
 
+//Global State
+//  -Search Object
+//  -Current Recipe Object
+//  -Shopping List Object
+//  -Liked Recipes
+const state = {};
+
+
 const search = new Search('pizza');
 console.log(search);
 search.getResults();
