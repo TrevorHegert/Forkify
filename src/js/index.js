927ec8d6d31339e542e1481880c06d4669fcf75b
Trevor Hegert <thegert138@gmail.com>
2019-01-22 17:24:18 -0500

    Building Recipe Model - controller reads hash

--- src/js/index.js
+++ src/js/index.js
@@ -63,6 +63,9 @@ elements.searchResultPages.addEventListener('click', e => {
 
 //RECIPE CONTROLLER
 
-const r = new Recipe(46956);
-r.getRecipe();
-console.log(r);
+const controlRecipe = () => {
+    const id = window.location.hash;
+    console.log(id);
+};
+
+window.addEventListener('hashchange', controlRecipe);
